56ef2eca990a8f395ec2a7f1b2fd61b28a956bc5
Druva Y <druva.flash@gmail.com>
2025-04-12 19:16:14 +0530

    try to set test env

---
+++ tryout/digit_recognizer/set_virtua_env.sh
@@ -0,0 +1,13 @@
+# Create a new folder for your project
+mkdir digit-recognizer
+cd digit-recognizer
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate it
+# Windows
+venv\Scripts\activate
+
+# macOS/Linux
+source venv/bin/activate
