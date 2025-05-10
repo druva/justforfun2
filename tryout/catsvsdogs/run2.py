7060587453a6cbe99c2a705b12a8f4a1047a851e
druva <druva.yarlagadda@xperi.com>
2025-05-10 17:57:14 +0530

    create a copy of run1.py

---
+++ tryout/catsvsdogs/run2.py
@@ -0,0 +1,154 @@
+from __future__ import absolute_import, division, print_function
+import os, subprocess
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+#download the data set
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+zip_dir = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+
+zip_dir_base = os.path.dirname(zip_dir)
+base_dir = os.path.join(zip_dir_base, 'cats_and_dogs_extracted/cats_and_dogs_filtered')
+print("zip_dir_base", zip_dir_base)
+print("base_dir", base_dir)
+#!find $zip_dir_base -type d -print
+zip_dir_base = "/your/path/here"  # make sure this is defined earlier in your script
+subprocess.run(["find", zip_dir_base, "-type", "d", "-print"])
+
+#create Folder Structures
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+print(train_dir)
+print(validation_dir)
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+print(train_cats_dir)
+print(train_dogs_dir)
+print(validation_cats_dir)
+print(validation_dogs_dir)
+
+num_cats_tr = len(os.listdir(train_cats_dir))
+num_dogs_tr = len(os.listdir(train_dogs_dir))
+num_cats_val = len(os.listdir(validation_cats_dir))
+num_dogs_val = len(os.listdir(validation_dogs_dir))
+total_train = num_cats_tr + num_dogs_tr
+total_val = num_cats_val + num_dogs_val
+print('total training cat images:', num_cats_tr)
+print('total training dog images:', num_dogs_tr)
+print('total validation cat images:', num_cats_val)
+print('total validation dog images:', num_dogs_val)
+print("--")
+print("Total training images:", total_train)
+print("Total validation images:", total_val)
+
+#prepare for training
+BATCH_SIZE = 100
+IMG_SHAPE = 150
+
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+
+
+#The actual rescaling
+train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_SHAPE,IMG_SHAPE), #(150,150)
+                                                           class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                              directory=validation_dir,
+                                                              shuffle=False,
+                                                              target_size=(IMG_SHAPE,IMG_SHAPE), #(150,150)
+                                                              class_mode='binary')
+
+
+#visualize images
+sample_training_images, _ = next(train_data_gen) 
+# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
+
+
+plotImages(sample_training_images[:5])
+
+
+#Create model with 4 convolution blocks with a maxpoolayer in between each
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(128, (3,3), activation = 'relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(128, (3,3), activation = 'relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax') #Cause it is either a 0 or 1 
+
+
+])
+#Compile model
+#if activation = softmax then compile loss with SparseCategoricalCrossentropy
+#if activation = sigmoid then compile loss with binary_crossenteropy
+model.compile(optimizer = 'adam', 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
+              metrics=['accuracy']
+                                    )
+
+#get a summary of the model
+model.summary()
+
+
+#Train model, use fitgenerator instead of fit since its coming from a generator
+EPOCHS = 40
+history = model.fit(
+    train_data_gen,
+    steps_per_epoch = int(np.ceil(total_train/float(BATCH_SIZE))),
+    epochs=EPOCHS,
+    validation_data=val_data_gen,
+    validation_steps=int(np.ceil(total_val/float(BATCH_SIZE)))
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.savefig('./foo.png')
+plt.show()
+
+#Next Steps is to do image augmentation for training
